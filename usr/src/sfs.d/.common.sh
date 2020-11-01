b49f046347a52acca24f49c1dce82007e5a3533c
Lauri Korts-Pärn <lauri@korc.jp>
2020-11-01 21:26:41 +0900

    add favorite link, check-up-to-date

--- usr/src/sfs.d/.common.sh
+++ usr/src/sfs.d/.common.sh
@@ -7,3 +7,13 @@
 : "${repo_key:=https://repo.vivaldi.com/archive/linux_signing_key.pub}"
 : "${repo:=https://repo.vivaldi.com/archive/deb/}"
 : "${repo_branch:=stable main}"
+: "${pkgs_gz_url:=$repo/dists/stable/main/binary-amd64/Packages.gz}"
+
+installed_ver() {
+  "$DESTDIR/opt/vivaldi/vivaldi" --version | cut -f2 -d" "
+}
+
+latest_ver() {
+  : "${pkgs_gz:=$(dl_file "$pkgs_gz_url")}"
+  zgrep -e "^Package: vivaldi-stable" -e Version "$pkgs_gz" | grep -A1 ^Package: | grep ^Version: | cut -f2 -d" " | cut -f1 -d-
+}
